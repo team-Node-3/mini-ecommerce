c8de277fdf475152db3df77868c0a47f127ddcc8
newbnodejs <newbnodejs@gmail.com>
2023-02-02 08:46:22 +0900

    orders.controller.js 초안 작성

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -3,17 +3,61 @@ const OrderService = require("../services/orders.service");
 class OrderController {
     orderService = new OrderService();
 
+
     // 장바구니 조회하기
-    getOrders
+    getOrders = async (req, res, next) => {
+        const { userId } = res.locals.user;
+
+        const getOrder = await this.orderService.findAllOrder(userId);
+
+        res.status(200).json({ data: getOrder });
+    }
+
 
     // 주문하기
-    purchaseOrder
+    purchaseOrder = async (req, res, next) => {
+        const { userId } = res.locals.user;
+        const { productId } = req.params;
+        const { quantity } = req.body;
+
+        const purchaseOrder = await this.orderService.purchaseOrder(
+            userId, 
+            productId,
+            quantity
+        );
+
+        res.status(200).json({ data: purchaseOrder });
+    }
+
 
     // 주문 수정
-    modifyOrder
+    modifyOrder = async (req, res, next) => {
+        const { userId } = res.locals.user;
+        const { productId } = req.params;
+        const { quantity } = req.body;
+
+        const modifyOrder = await this.orderService.modifyOrder(
+            userId,
+            productId,
+            quantity
+        );
+
+        res.status(200).json({ data: modifyOrder });
+    }
+
 
     // 주문 삭제
-    deleteOrder
+    deleteOrder = async (req, res, next) => {
+        const { userId } = res.locals.user;
+        const { productId } = req.params;
+
+        const deleteOrder = await this.orderService.deleteOrder(
+            userId,
+            productId
+        );
+
+        res.status(200).json({ data: deleteOrder });
+    }
 }
 
 module.exports = OrderController;
