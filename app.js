549131fb090569e045dea33bfba999e9bb205bb6
JUNHYEOK CHOI <junhyeokchoi@JUNHYEOKui-MacBookAir.local>
2023-02-02 17:48:50 +0900

    어드민페이지 상품등록/수정/삭제 기능

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
+
 dotenv.config()
 
 const app = express()
+
+
+const productsRouter = require("./routes/products.Router.js")
+
+app.set('view engine', "ejs")
+app.set("views", "./views")
+
+app.use(express.static("./assets"));
 app.use(express.json())
+
+
+app.use("/admin", productsRouter);
+
+
 app.get('/', (req, res) => {
   res.send('HiHi')
+
+
 })
-app.listen(process.env.port, async () => {
+app.listen(3000, async () => {
   console.log('server started!')
 })
