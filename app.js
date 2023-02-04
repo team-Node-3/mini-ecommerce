5990dc01eee5293a7057b9705888c034f05c9af2
devneken <devneken@gmail.com>
2023-02-04 11:28:13 +0900

    어드민페이지 dev 병합

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require('express')
+const dotenv = require('dotenv')
 
 const app = express();
 dotenv.config();
 
 const authRouter = require('./routes/auth.routes')
 const mypageRouter = require('./routes/mypage.routes');
+const adminRouter = require("./routes/admin.Router.js")
 
+app.set('view engine', "ejs")
+app.set("views", "./views")
+
+
+app.set('view engine', "ejs")
+app.set("views", "./views")
+
+app.use(express.static("./assets"));
 // const cookieParser = require('cookie-parser')
-
-// 라우터
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use('/auth', authRouter);
 app.use('/mypage', mypageRouter);
+app.use('/admin', adminRouter);
 
-// 포트
+app.get('/', (req, res) => {
+  res.send('HiHi')
+})
+
 app.listen(process.env.port, async () => {
   console.log('server started!')
 })
