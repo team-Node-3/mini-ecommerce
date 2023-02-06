ffca60f4bbec4140c97310e225d7ad13ed884582
JeremyKang <castleownerkang@gmail.com>
2023-02-06 16:58:34 +0900

    first

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -10,7 +10,7 @@ class AuthController {
   // 회원가입
   signup = async (req, res, next) => {
     try {
-      const { email, pw, nickName, PhoneNumber, address, admin } = req.body;
+      const { email, password, name, nickname, phone, address, admin } = req.body;
 
       // if( email || pw || nickName || PhoneNumber || address || admin ) {
       //   return res.status(400).json({ message: "값을 모두 입력하세요"})
@@ -18,20 +18,21 @@ class AuthController {
     
 
      // 닉네임 중복체크
-    const foundBynickName = await this.authRepository.findBynickName(nickName);
+    const foundBynickName = await this.authRepository.findBynickName(nickname);
 
     if(foundBynickName.length > 0) {
-      return res.status(409).json({ message: `${nickName} already exists`})
+      return res.status(409).json({ message: `${nickname} already exists`})
     } 
 
     // 비밀번호 암호화(bcrypt)고 salt는 12로 설정
-    const hashed = await bcrypt.hash(pw, 12);
+    const hashed = await bcrypt.hash(password, 12);
 
     const createUser = await this.authRepository.createUser(
       email,
       hashed,
-      nickName,
-      PhoneNumber,
+      name,
+      nickname,
+      phone,
       address,
       admin
     );
@@ -48,14 +49,14 @@ class AuthController {
   // 로그인
   login = async (req, res, next) => {
     try {
-      const { nickName, pw } = req.body;
+      const { nickname, password } = req.body;
 
       // 닉네임으로 검색
-      const user = await this.authRepository.findBynickName(nickName);
-
+      const user = await this.authRepository.findBynickName(nickname);
+      console.log(user)
       // 입력 pw와 DB의 pw일치여부 판단
-      const pwTest = await bcrypt.compare(pw, user[0].pw)
-
+      const pwTest = await bcrypt.compare(password, user[0].password)
+      console.log(pwTest)
    // pw 불일치 혹은 user검색결과가 없을때
 
       if(user.length === 0 || !pwTest ){
@@ -67,9 +68,10 @@ class AuthController {
     //   // 토큰 생성
       const accessToken = jwt.sign(
         {
-    //       // userId 와 nickName을 토큰으로 만듬
+    //       // userId 와 nickName, admin을 토큰으로 만듬
           userId: user[0].id,
-          userNickname: user[0].nickName,
+          userNickname: user[0].nickname,
+          admin: user[0].admin,
         },
         "my-secret-key", //비밀키
         { expiresIn: "1d"} // 유효기간 1일
