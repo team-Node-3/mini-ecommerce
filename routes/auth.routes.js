cfb8d4b0ff636b5aa923f05f2245ad495b7a3e43
JeremyKang <castleownerkang@gmail.com>
2023-02-06 09:07:31 +0900

    front

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,3 +1,6 @@
+const { body } = require('express-validator')
+const validate = require('../middleware/validateSignup')
+
 const express = require('express');
 const router = express.Router();
 
@@ -5,7 +8,22 @@ const AuthController = require('../controllers/auth.controller')
 const authController = new AuthController();
 const authMiddleware = require('../middleware/auth')
 
-router.post("/signup", authController.signup);
+const validateCredential = [
+  body('email')
+    .trim()
+    .notEmpty()
+    .isEmail()
+    .withMessage('Please enter valid email address'),
+  body('pw')
+    .trim()
+    .isLength({ min: 3 }, { max: 20 })
+    .withMessage('password should be at least 3 characters'),
+  body('admin')
+    .notEmpty(),
+  validate,
+];
+
+router.post("/signup", validateCredential, authController.signup);
 
 router.post("/login", authController.login);
 
